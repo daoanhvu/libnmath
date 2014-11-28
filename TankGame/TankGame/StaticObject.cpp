2c5ed299966455bce958500900ed3b27bbf32df1
daoanhvu122 <daoanhvu122@gmail.com>
2014-11-28 21:03:24 +0700

    StaticObject

--- TankGame/TankGame/StaticObject.cpp
+++ TankGame/TankGame/StaticObject.cpp
@@ -14,6 +14,8 @@ StaticObject::~StaticObject() {
 int StaticObject::load(short *indices, int indexSize, float *vertices, int size, int sizePerVertex, int posOffs, int colorOffs, int normalOffs) {
 	glGenBuffers(2, mBufferId);
 
+	GLsizei stride = 4 * (posOffs + colorOffs + normalOffs); 
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mBufferId[0]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexSize * 2, indices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -21,9 +23,15 @@ int StaticObject::load(short *indices, int indexSize, float *vertices, int size,
 	glBindBuffer(GL_ARRAY_BUFFER, mBufferId[1]);
 	glBufferData(GL_ARRAY_BUFFER, size * 4, vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, mBufferId[1]);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, 0);
+
+	
 	return 0;
 }
 
 void StaticObject::render() {
-
+	glDrawArrays(mPrimitiveType, 0, );
 }
