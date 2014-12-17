59e6ee989eca28eec12278f21f73207894d3d776
daoanhvu <daoanhvu122@gmail.com>
2014-12-17 08:05:05 +0700

    Plotter

--- Plotter/FPlotter/FPlotter/modelgl.cpp
+++ Plotter/FPlotter/FPlotter/modelgl.cpp
@@ -9,29 +9,20 @@
 #include "modelgl.h"
 
 using namespace std;
+using namespace nmath;
 
-ModelGL::ModelGL(): mFunction(NULL), data(NULL) {
+ModelGL::ModelGL(): mFunction(NULL) {
 	bgColor[0] = bgColor[1] = bgColor[2] = bgColor[3] = 0;
 }
 
 ModelGL::~ModelGL() {
+	release();
 }
 
 void ModelGL::release() {
 	int i;
 	if(mFunction != NULL) {
-		releaseFunct(mFunction);
-		free(mFunction);
-	}
-
-	if(data != NULL) {
-		for(i=0; i<data->size; i++) {
-			free(data->list[i]->data);
-			free(data->list[i]->rowInfo);
-			free(data->list[i]);
-		}
-		free(data->list);
-		free(data);
+		delete mFunction;
 	}
 }
 
@@ -40,24 +31,15 @@ void ModelGL::initLights() {
 
 void ModelGL::createObject() {
 	float bd[4] = {-2, 2, -2, 2};
-	mFunction = (Function*)malloc(sizeof(Function));
-	mFunction->prefix = NULL;
-	mFunction->domain = NULL;
-	mFunction->criterias = NULL;
-	mFunction->str = NULL;
-	mFunction->len = 0;
-	mFunction->variableNode = NULL;
-	mFunction->numVarNode = 0;
-	mFunction->valLen = 0;
-
-	parseFunction("f(x,y)=sin(x)+cos(y)", 20, mFunction);
-	if(getErrorCode() != NMATH_NO_ERROR) {
-		releaseFunct(mFunction);
-		free(mFunction);
+	mFunction = new NFunction();
+	
+	mFunction->parse("f(x,y)=sin(x)+cos(y)", 20);
+	if(mFunction->getErrorCode() != NMATH_NO_ERROR) {
+		delete mFunction;
 		return;
 	} 
 
-	data = getSpaces(mFunction, bd, mFunction->valLen * 2, 0.1f);
+	ListFData *data = mFunction->getSpace(bd, 0.2f, false);
 }
 
 void ModelGL::init() {
