a468597a05f754156490d1d74b43c8bbab16e2ee
daoanhvu <daoanhvu122@gmail.com>
2015-02-04 18:13:18 +0700

    Floating point

--- cpp2/fp128.h
+++ cpp2/fp128.h
@@ -2,7 +2,12 @@
 #define _FP128_H
 
 /*
-	To calculate the bias for an arbitrary sized floating point number apply the formula 2k−1 − 1 where k is the number of bits in the exponent.[1]
+
+http://www.rfwireless-world.com/Tutorials/floating-point-tutorial.html
+http://www.cs.umd.edu/class/sum2003/cmsc311/Notes/BinMath/addFloat.html
+http://www.ecs.umass.edu/ece/koren/arith/simulator/FPAdd/
+
+	To calculate the bias for an arbitrary sized floating point number apply the formula 2^(k−1) − 1 where k is the number of bits in the exponent.[1]
 
 When interpreting the floating-point number, the bias is subtracted to retrieve the actual exponent.
 
@@ -13,17 +18,52 @@ For a quad-precision number, an exponent in the range −16382 .. +16383 is bias
 Referrences:
 O'Hallaron, Randal E. Bryant, David R. (2010). Computer systems : a programmer's perspective (2nd ed. ed.). Boston: Prentice Hall. ISBN 978-0-13-610804-7.
 
+	binary: |sign (1bit)|exponent(21 bits)|fraction|
+
+	|sign (1bit)|exponent(21 bits)|mantissa|
 	Here I use 21 bit for exponent
 */
 #define BIAS 1048575
 
 class FP128 {
 	private:
-		unsigned char data[16];
+		unsigned char sign;
+		unsigned int mantissa[8];
+		int exp;
 
 	public:
 		FP128();
 		FP128(double value);
+
+		int getExponent() { return exp; }
+
+		FP128 operator =(FP128 &a);
+
+		unsigned char operator >(float a);
+		int operator <(float a);
+		int operator >=(float a);
+		int operator <=(float a);
+		int operator !=(float a);
+		int operator ==(float a);
+
+		int operator >(double a);
+		int operator <(double a);
+		int operator >=(double a);
+		int operator <=(double a);
+		int operator !=(double a);
+		int operator ==(double a);
+
+		int operator >(FP128 &a);
+		int operator <(FP128 &a);
+		int operator >=(FP128 &a);
+		int operator <=(FP128 &a);
+		int operator !=(FP128 &a);
+		int operator ==(FP128 &a);
+
+		friend FP128 operator +(const FP128 &a, const FP128 &b);
+		friend FP128 operator -(const FP128 &a, const FP128 &b);
+		friend FP128 operator *(const FP128 &a, const FP128 &b);
+		friend FP128 operator /(const FP128 &a, const FP128 &b);
 };
 
 #endif
